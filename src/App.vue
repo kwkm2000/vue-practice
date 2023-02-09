ce5d0c3e31fdde7bf7f0853235565254ac5087bf
kwkm2000 <kwkm.0802@gmail.com>
2023-02-09 19:14:35 +0900

    TODOの実装

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <form @submit="onSubmit">
+    <input type="text" @input="onInput" />
+  </form>
+
+  <ul>
+    <li v-for="task in tasks" :key="task.id">
+      {{ task.text }} <input type="checkbox" :checked="task.isDone" />
+      <button @click="onClick($event, task.id)">✗</button>
+    </li>
+  </ul>
+  <p>task length {{ tasks.length }}</p>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref } from "vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const items = [
+  { id: 1, text: "task1", isDone: false },
+  { id: 2, text: "task2", isDone: true },
+];
+const tasks = ref(items);
+const text = ref("");
+const onSubmit = (e) => {
+  e.preventDefault();
+
+  const newTask = {
+    id: tasks.value.length + 1,
+    text,
+    isDone: false,
+  };
+
+  tasks.value.unshift(newTask);
+};
+const onInput = (e) => {
+  const newText = e.target.value;
+
+  text.value = newText;
+};
+const onClick = (e, taskId) => {
+  e.preventDefault();
+  console.log(taskId);
+  const filteredTasks = tasks.value.filter((task) => task.id !== taskId);
+  tasks.value = filteredTasks;
+};
 </script>
 
 <style>
+* {
+  /* margin: 0; */
+  /* padding: 0; */
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: sans-serif;
+  text-align: left;
   margin-top: 60px;
+}
+li {
+  list-style: none;
 }
 </style>
